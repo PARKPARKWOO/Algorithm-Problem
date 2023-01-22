97ebff321e49f38db15050c4c43a367b008ae1ad
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-01-22 23:45:00 +0900

    [Silver I] Title: 회의실 배정, Time: 252 ms, Memory: 51448 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,17 @@
+import sys
+input = sys.stdin.readline
+n = int(input())
+time_list = []
+for i in range(n):
+    start, end = map(int, input().split())
+    time_list.append((start, end))
+
+time_list.sort(key=lambda x: (x[1], x[0]))
+
+cnt = 1
+end_time = time_list[0][1]
+for i in range(1, n):
+    if time_list[i][0] >= end_time:
+        cnt += 1
+        end_time = time_list[i][1]
+print(cnt)
