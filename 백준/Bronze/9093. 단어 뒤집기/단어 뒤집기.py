9022d41d109f7f1b94009e76097fb5996d64f8f1
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-01-23 14:35:46 +0900

    [Bronze I] Title: 단어 뒤집기, Time: 788 ms, Memory: 30616 KB -BaekjoonHub

--- 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
+++ 백준/Bronze/9093. 단어 뒤집기/단어 뒤집기.py
@@ -1,12 +1,5 @@
 t = int(input())
 for i in range(t):
-    s = input()
-    s += ' '
-    stack = []
+    s = list(input().split())
     for j in s:
-        if j != " ":
-            stack.append(j)
-        else:
-            while stack:
-                print(stack.pop(), end='')
-            print(end=' ')
+        print(j[::-1], end=' ')
