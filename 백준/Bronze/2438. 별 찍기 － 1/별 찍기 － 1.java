2c91e373ddcb379217cc4f6f716cc11bfe9dea52
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-03-05 22:31:12 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 352 ms, Memory: 20156 KB -BaekjoonHub

---
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int num = scan.nextInt();
+        for (int i = 1; i <= num; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.printf("*");
+            }
+            System.out.println();
+        }
+    }
+}
