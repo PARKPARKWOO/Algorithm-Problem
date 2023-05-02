bffef2093751b2b8aa88b377b9270459530790d8
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-05-02 10:01:56 +0900

    [level 2] Title: 피보나치 수, Time: 15.88 ms, Memory: 81 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.java
@@ -9,7 +9,7 @@ class Solution {
         for (int i = 3; i < n+1; i++) {
             list.add((list.get(i-1) + list.get(i - 2)) % 1234567);
         }
-        answer = list.get(n) % 1234567;
+        answer = list.get(n);
         return answer;
     }
 }
