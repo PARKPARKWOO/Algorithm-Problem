5beadee4bc4fa597ebe131ced5b34e11cce0cdbd
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-05-09 10:48:30 +0900

    [level 2] Title: 숫자의 표현, Time: 1.13 ms, Memory: 52.1 MB -BaekjoonHub

--- 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
+++ 프로그래머스/lv2/12924. 숫자의 표현/숫자의 표현.java
@@ -1,24 +1,6 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-
-        for (int i = 1; i <= n; i++) {
-            int temp = i;
-            if (temp == n) {
-                answer++;
-                continue;
-            }
-            for (int j = i + 1; j <= n; j++) {
-                temp += j;
-                if (temp > n) {
-                    break;
-                } else if (temp < n) {
-                    continue;
-                } else {
-                    answer++;
-                }
-            }
-        }
         return answer;
     }
 }
