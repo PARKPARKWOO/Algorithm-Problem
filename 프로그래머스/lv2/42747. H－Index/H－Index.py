27c9e9169354ccf89ad56cae2c074c4b91789f0b
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-05-28 23:15:39 +0900

    [level 2] Title: H-Index, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42747. H－Index/H－Index.py
@@ -0,0 +1,7 @@
+def solution(citations):
+    citations.sort(reverse=True)
+    for idx, citation in enumerate(citations):
+        print(idx, citation)
+        if idx >= citation:
+            return idx
+    return len(citations)
