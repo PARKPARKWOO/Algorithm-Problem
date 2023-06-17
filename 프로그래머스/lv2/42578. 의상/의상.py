2360a5df534c6cbc676b4a8fc2bae387db301fad
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-06-17 20:23:59 +0900

    [level 2] Title: 의상, Time: 0.03 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/lv2/42578. 의상/의상.py
+++ 프로그래머스/lv2/42578. 의상/의상.py
@@ -1,9 +1,6 @@
+from collections import Counter
+from functools import reduce
 def solution(clothes):
-    hash_map = {}
-    answer = 1
-    for clothe, type in clothes:
-        hash_map[type] = hash_map.get(type, 0) + 1
-    
-    for type in hash_map:
-        answer *= hash_map[type] + 1
-    return answer - 1
+    counter = Counter([type for clothe, type in clothes])
+    answer = reduce(lambda acc, cur: acc*(cur + 1), counter.values(), 1) - 1
+    return answer
