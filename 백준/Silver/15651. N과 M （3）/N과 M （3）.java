4de76290765f301f89b03f45e6c02ffb074b5209
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2023-08-02 00:00:43 +0900

    [Silver III] Title: N과 M (3), Time: 1524 ms, Memory: 437228 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class Main {
+    static int[] sel;
+    static int M,N;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        sel = new int[M];
+        recursive (1,0);
+        System.out.println(sb.toString().replace("[","").replace("]","").replace(",",""));
+    }
+
+    static void recursive(int idx, int k) {
+        if (k == M) {
+//            System.out.println(Arrays.toString(sel).replace("[","").replace("]","").replace(",",""));
+            sb.append(Arrays.toString(sel));
+            sb.append("\n");
+            return;
+        }
+        for (int i = idx; i <= N; i++) {
+            sel[k] = i;
+            recursive(idx, k + 1);
+        }
+
+    }
+}
