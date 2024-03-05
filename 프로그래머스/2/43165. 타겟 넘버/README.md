98a43fd5cf0acffed1d57be9ea6f32c4f246c1b6
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-03-06 02:48:13 +0900

    [level 2] Title: 타겟 넘버, Time: 0.40 ms, Memory: 71.8 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/README.md
+++ 프로그래머스/2/43165. 타겟 넘버/README.md
@@ -1,10 +1,10 @@
 # [level 2] 타겟 넘버 - 43165 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43165) 
 
 ### 성능 요약
 
-메모리: 78.6 MB, 시간: 0.45 ms
+메모리: 71.8 MB, 시간: 0.40 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2023년 12월 0일 22:8:7
+2024년 03월 06일 02:48:12
 
 ### 문제 설명
 
--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,28 +1,19 @@
 class Solution {
-    private boolean[] v;
     private int result;
     public int solution(int[] numbers, int target) {
-        int answer = 0;
-        v = new boolean[numbers.length];
         result = 0;
-        dfs(numbers, 0, 0, target, numbers.length);
-        answer = result;
-        System.out.println(answer);
-        return answer;
+        dfs(target, 0, numbers, 0);
+        return result;
     }
-
-    // 현재 값 k, 끝 end
-    private void dfs(int[] numbers, int idx, int k, int target, int end) {
-        if (idx > end) return;
-
-        if (idx == end) {
-            if (k == target) {
+    
+    private void dfs(int target, int n, int[] numbers, int idx) {
+        if (idx == numbers.length) {
+            if (target == n) {
                 result++;
             }
             return;
         }
-
-        dfs(numbers, idx + 1, k + numbers[idx], target, end);
-        dfs(numbers, idx + 1, k - numbers[idx], target, end);
+        dfs(target, n + numbers[idx], numbers, idx + 1);
+        dfs(target, n - numbers[idx], numbers, idx + 1);
     }
 }
