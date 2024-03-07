c055e592cef8c636a8b71f295891b9991606de0e
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-03-08 02:16:10 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 6.17 ms, Memory: 56 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,54 +1,43 @@
 import java.util.*;
 class Solution {
-    private int[] dx = {-1, 1, 0, 0};
+    private int[] dx = {-1, 1, 0, 0}; // 상 하 좌 우
     private int[] dy = {0, 0, -1, 1};
-    private int ans;
+    private boolean[][] v;
+    private int result;
     public int solution(int[][] maps) {
         int answer = 0;
-        ans = Integer.MAX_VALUE / 2;
-        bfs(maps);
-        answer = ans == Integer.MAX_VALUE / 2 ? -1 : ans;
-        return answer;
+        result = 0;
+        int x = maps.length;
+        int y = maps[0].length;
+        v = new boolean[x][y];
+        bfs(maps, x, y);
+        return maps[x - 1][y - 1] == 1 ? -1 : maps[x-1][y-1];
     }
-
-    class Node {
-        int x, y, cnt;
-
-        public Node(int x, int y, int cnt) {
-            this.x = x;
-            this.y = y;
-            this.cnt = cnt;
-        }
-    }
-
-    private void bfs(int[][] maps) {
-        int maxX = maps.length;
-        int maxY = maps[0].length;
-        boolean[][] v = new boolean[maxX][maxY];
-        for (int i = 0; i < maxX; i++) {
-            for (int j = 0; j < maxY; j++) {
-                v[i][j] = false;
-            }
-        }
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0, 0, 1));
-        v[0][0] = true;
-        while (!queue.isEmpty()) {
-            Node poll = queue.poll();
+    private void bfs(int[][] maps, int xlen, int ylen) {
+        Queue<Nodes> q = new LinkedList<>();
+        q.add(new Nodes(0, 0));
+        while (!q.isEmpty()) {
+            Nodes poll = q.poll();
             int x = poll.x;
             int y = poll.y;
-            int cnt = poll.cnt;
-            if (x == maxX -1 && y == maxY - 1) {
-                ans = Math.min(cnt, ans);
-            }
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (nx < maxX && nx >= 0 && ny < maxY && ny >= 0 && maps[nx][ny] == 1 && !v[nx][ny]) {
-                    queue.add(new Node(nx, ny, cnt + 1));
+                if (nx >= 0 && nx < xlen && ny >= 0 && ny < ylen && !v[nx][ny] && maps[nx][ny] != 0) {
+                    q.add(new Nodes(nx, ny));
                     v[nx][ny] = true;
+                    maps[nx][ny] = maps[x][y] + 1;
                 }
             }
         }
+    }
+}
+class Nodes {
+    int x;
+    int y;
+    public Nodes(int x, int y) {
+        this.x = x;
+        this.y = y;
+    
     }
 }
