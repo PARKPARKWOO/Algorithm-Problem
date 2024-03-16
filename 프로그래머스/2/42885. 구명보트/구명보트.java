6c59799ddbe64bd0c1261d475e2d1d947bc973b7
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-03-16 19:41:20 +0900

    [level 2] Title: 구명보트, Time: 9.25 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,12 +1,12 @@
-import java.util.*;
+import java.util.*; 
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int minIdx = 0;
-        for (int i = people.length - 1; minIdx <= i; i--){
-            if (people[i] + people[minIdx] <= limit) {
-                minIdx++;
+        int start = 0;
+        for (int end = people.length - 1; start <= end; end--) {
+            if (limit >= people[start] + people[end]) {
+                start++;
             }
             answer++;
         }
