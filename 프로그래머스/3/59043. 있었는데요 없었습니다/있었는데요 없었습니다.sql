acc3f138c767de5dc2f707ed0e3394ae89e2276c
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-05-03 23:00:25 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -0,0 +1,8 @@
+-- 코드를 입력하세요
+SELECT ai.ANIMAL_ID, ai.NAME
+from ANIMAL_INS as ai
+left join
+ANIMAL_OUTS as ao
+on ai.ANIMAL_ID = ao.ANIMAL_ID
+where ai.DATETIME > ao.DATETIME
+order by ai.DATETIME ;
