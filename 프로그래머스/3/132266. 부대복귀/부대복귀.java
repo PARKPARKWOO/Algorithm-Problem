0959468d9ba7fb68b407ccd38fff781ee1269e60
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-06-02 01:30:10 +0900

    [level 3] Title: 부대복귀, Time: 50.65 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,45 +1,51 @@
 import java.util.*;
+
 class Solution {
-    private ArrayList<ArrayList<Integer>> list;
-    private int[] dis;
+    private List<List<Integer>> graph;
+    
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
+        graph = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int[] road : roads) {
+            int from = road[0];
+            int to = road[1];
+            graph.get(from).add(to);
+            graph.get(to).add(from);
+        }
+        
+        // 목적지에서 모든 노드까지의 최단 거리를 계산
+        int[] distances = bfsFromDestination(n, destination);
+        
         int[] answer = new int[sources.length];
-        list = new ArrayList<>();
-        dis = new int[n];
-        // list 생성 없는 곳은 empty
-        for (int i = 0; i < n; i++) {
-            list.add(new ArrayList<>());
-        }
-        // init
-        for (int i = 0; i < roads.length; i++) {
-            int from = roads[i][0] - 1;
-            int to = roads[i][1] - 1;
-            list.get(from).add(to);
-            list.get(to).add(from);
-        }
-        Arrays.fill(dis, Integer.MAX_VALUE);
-        bfs(destination - 1);
         for (int i = 0; i < sources.length; i++) {
-            int sourceIndex = sources[i] - 1; // sources 배열의 값들을 올바르게 인덱싱
-            answer[i] = dis[sourceIndex] == Integer.MAX_VALUE ? -1 : dis[sourceIndex];
+            answer[i] = distances[sources[i]];
         }
+        
         return answer;
     }
-    private int bfs(int destination) {
+    
+    private int[] bfsFromDestination(int n, int destination) {
+        int[] distances = new int[n + 1];
+        Arrays.fill(distances, -1);
         Queue<Integer> q = new LinkedList<>();
-        dis[destination] = 0;
         q.add(destination);
-        int result = -1;
+        distances[destination] = 0;
+        
         while (!q.isEmpty()) {
-            Integer poll = q.poll();
-            for (int i = 0; i < list.get(poll).size(); i++) {
-                int t = list.get(poll).get(i);
-                if (dis[poll] + 1 < dis[t]) {
-                    dis[t] = dis[poll] + 1;
-                    q.add(t);
+            int current = q.poll();
+            int currentDistance = distances[current];
+            
+            for (int neighbor : graph.get(current)) {
+                if (distances[neighbor] == -1) { // 방문하지 않은 노드
+                    distances[neighbor] = currentDistance + 1;
+                    q.add(neighbor);
                 }
             }
         }
-        return result;
+        
+        return distances;
     }
+
 }
