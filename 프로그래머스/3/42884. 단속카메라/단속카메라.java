9bf92a9a179f98eb55a1b9d5b5d9f367ebe42a58
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-06-02 00:10:31 +0900

    [level 3] Title: 단속카메라, Time: 10.52 ms, Memory: 53.8 MB -BaekjoonHub

--- 프로그래머스/3/42884. 단속카메라/단속카메라.java
+++ 프로그래머스/3/42884. 단속카메라/단속카메라.java
@@ -1,15 +1,19 @@
 import java.util.*;
 class Solution {
     public int solution(int[][] routes) {
-        int answer = 0;
         Arrays.sort(routes, (a, b) -> Integer.compare(a[1], b[1]));
-        int temp = Integer.MIN_VALUE;
-        for (int[] arr : routes) {
-            if (temp < arr[0]) {
-                temp = arr[1];
+        
+        int answer = 0;
+        int camera = Integer.MIN_VALUE; // 현재 설치된 카메라의 위치를 나타내는 변수
+
+        for (int[] route : routes) {
+            // 현재 차량의 진입 지점이 마지막 카메라 위치보다 뒤에 있으면 새로운 카메라 설치 필요
+            if (camera < route[0]) {
                 answer++;
+                camera = route[1]; // 새로운 카메라를 차량의 진출 지점에 설치
             }
         }
+
         return answer;
     }
 }
