5f12fa2f12d9bd8bbfee9bbaac42332fef9eb4b1
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-06-06 23:50:56 +0900

    [level 3] Title: 순위, Time: 17.73 ms, Memory: 85.2 MB -BaekjoonHub

--- 프로그래머스/3/49191. 순위/순위.java
+++ 프로그래머스/3/49191. 순위/순위.java
@@ -1,37 +1,33 @@
 class Solution {
     public int solution(int n, int[][] results) {
+        int answer = 0;
         int[][] graph = new int[n + 1][n + 1];
-        for (int[] edge : results) {
-            graph[edge[0]][edge[1]] = 1;
-            graph[edge[1]][edge[0]] = -1;
+        for (int[] result : results ) {
+            int win = result[0];
+            int lose = result[1];
+            graph[win][lose] = 1;
+            graph[lose][win] = -1;
         }
-
-        // 플로이드-워셜 알고리즘
-        for (int k = 1; k <= n; k++) {
-            for (int i = 1; i <= n; i++) {
-                for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j =1; j <=n; j++) {
+                for (int k = 1; k <=n; k++) {
                     if (graph[i][k] == 1 && graph[k][j] == 1) {
                         graph[i][j] = 1;
                         graph[j][i] = -1;
-                    }
-                    if (graph[i][k] == -1 && graph[k][j] == -1) {
+                    } else if (graph[i][k] == -1 && graph[k][j] == -1) {
                         graph[i][j] = -1;
                         graph[j][i] = 1;
                     }
                 }
             }
         }
-
-        int answer = 0;
-
         for (int i = 1; i <= n; i++) {
-            int count = 0;
+            int cnt = 0;
             for (int j = 1; j <= n; j++) {
-                if (graph[i][j] != 0) count++;
+                if (graph[i][j] != 0) cnt++;
             }
-            if (count == n - 1) answer++;
+            if (cnt == n - 1) answer++;
         }
-
         return answer;
     }
 }
