39cd925c83382eb179cb3f19605aa27848be6d8e
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-08-31 16:42:31 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 36.43 ms, Memory: 88 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,34 +1,36 @@
 import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(0);
-        map.put(0, 1);
-        int flag = truck_weights[0];
-        int idx = 1;
-        int second = 1;
-        while (!queue.isEmpty()) {
-            second++;
-            int size = queue.size();
-            for (int i = 0; i < size; i++) {
-                Integer poll = queue.poll();
-                if (map.get(poll) < bridge_length) {
-                    map.put(poll, map.getOrDefault(poll, 0) + 1);
-                    queue.add(poll);
+        int time = 0;
+        int bridgeWeight = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+
+        for (int i = 0; i < truck_weights.length; i++) {
+            int truck = truck_weights[i];
+            while (true) {
+                if (bridge.isEmpty()) {
+                    bridge.add(truck);
+                    bridgeWeight += truck;
+                    time++;
+                    break;
+                } else if (bridge.size() == bridge_length) {
+                    bridgeWeight -= bridge.poll();
                 } else {
-                    flag -= truck_weights[poll];
+                    if (bridgeWeight + truck <= weight) {
+                        bridge.add(truck);
+                        bridgeWeight += truck;
+                        time++;
+                        break;
+                    } else {
+                        bridge.add(0);
+                        time++;
+                    }
                 }
             }
-            if (idx < truck_weights.length && weight >= flag + truck_weights[idx]) {
-                flag += truck_weights[idx];
-                queue.add(idx);
-                map.put(idx, map.getOrDefault(idx, 0) + 1);
-                idx++;
-            }
         }
-        
-        return answer = second;
+
+        time += bridge_length;
+
+        return time;
     }
 }
