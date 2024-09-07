fdd01c9e46e64b3338f75cd7123ffa42a0b9798d
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-09-07 13:28:44 +0900

    [level 2] Title: 택배상자, Time: 145.70 ms, Memory: 152 MB -BaekjoonHub

--- 프로그래머스/2/131704. 택배상자/택배상자.java
+++ 프로그래머스/2/131704. 택배상자/택배상자.java
@@ -3,34 +3,15 @@ class Solution {
     public int solution(int[] order) {
         int answer = 0;
         Stack<Integer> stack = new Stack<>();
-        int num = 1;
         int idx = 0;
-        while (order.length > idx) {
-            int now = order[idx];
-            // 배열에서 꺼낼때 같음
-            if (now == num) {
+        for (int i = 1; i <= order.length; i++) {
+            stack.add(i);
+            while (!stack.isEmpty() && stack.peek() == order[idx]) {
+                stack.pop();
                 answer++;
                 idx++;
-                num++;
-            } else {
-                // 보조 레일에서 꺼낸 값이랑 같으면
-                if (!stack.isEmpty() && stack.peek() == now) {
-                    stack.pop();
-                    idx++;
-                    answer++;
-                } else if (num < now ) {
-                    stack.add(num++);
-                } else if(!stack.isEmpty() && stack.peek() < now) {
-                    stack.add(num++);
-                } else if (stack.isEmpty() && now > num){
-                    stack.add(num++);
-                } else {
-                    break;
-                }
-                
             }
         }
         return answer;
     }
 }
-
