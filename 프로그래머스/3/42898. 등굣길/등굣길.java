a4758a36c0e7e7ce43ff282f8fa438f205a4a92e
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-09-13 22:16:41 +0900

    [level 3] Title: 등굣길, Time: 1.17 ms, Memory: 52.8 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,20 +1,37 @@
-import java.util.*;
+import java.util.Arrays;
+
 class Solution {
+    private int[][] dp;
+    int I = 1_000_000_007;
+
     public int solution(int m, int n, int[][] puddles) {
-        int answer = 0;
-        int a = 1000000007;
-        int[][] dp = new int[n + 1][m + 1];
-        for (int[] i: puddles) {
-            dp[i[1]][i[0]] = -1;
+        dp = new int[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], -1);
         }
-        dp[1][1] = 1;
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= m; j++) {
-                if (dp[i][j] == -1) continue;
-                if (dp[i - 1][j] > 0) dp[i][j] += dp[i - 1][j] % a;
-                if (dp[i][j - 1] > 0) dp[i][j] += dp[i][j - 1] % a;
-            }
+        
+        for (int[] puddle : puddles) {
+            dp[puddle[1] - 1][puddle[0] - 1] = 0;
         }
-        return dp[n][m] % a;
+
+        return findPaths(n - 1, m - 1);
+    }
+
+    private int findPaths(int y, int x) {
+        if (y == 0 && x == 0) {
+            return 1;
+        }
+
+        if (y < 0 || x < 0 || dp[y][x] == 0) {
+            return 0;
+        }
+
+        if (dp[y][x] != -1) {
+            return dp[y][x];
+        }
+
+        dp[y][x] = (findPaths(y - 1, x) + findPaths(y, x - 1)) % I;
+        return dp[y][x];
     }
 }
