80dee1e00b58d5aed271d169223a7d7f5a1195f1
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-10-26 20:43:52 +0900

    [Gold IV] Title: 최단경로, Time: 1948 ms, Memory: 324876 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,75 +1,58 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.PriorityQueue;
-import java.util.Scanner;
-
+import java.util.*;
 
 public class Main {
-	static class Vertex implements Comparable<Vertex>{
-		int e, w;
-		public Vertex(int e, int w) {
-			this.e = e;
-			this.w = w;
-		}
-		@Override
-		public int compareTo(Vertex o) {
-			// TODO Auto-generated method stub
-			return Integer.compare(this.w, o.w);
-		}
-		
-	}
-	static int V, E, K;
-	static int[] dist;
-	static ArrayList<Vertex> vertexs[];
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		V = sc.nextInt();
-		E = sc.nextInt();
-		K = sc.nextInt();
-		dist = new int[V + 1];
-		vertexs = new ArrayList[V + 1];
-		for (int i = 0; i <= V; i++) {
-			vertexs[i] = new ArrayList<Vertex>();
-		}
-		
-		for (int i = 0; i < E; i++) {
-			int s = sc.nextInt();
-			int e = sc.nextInt();
-			int w = sc.nextInt();
-			vertexs[s].add(new Vertex(e, w));
-		}
-		
-		dist = new int[V + 1];
-		Arrays.fill(dist, Integer.MAX_VALUE);
-		boolean[] v = new boolean[V + 1];
+    private static int V;
+    private static int E;
+    private static int K;
+    private static List<List<Edge>> graph;
 
-		dijkstra(K);
-		for (int i = 1; i < dist.length; i++) {
-			if (dist[i]== Integer.MAX_VALUE ) {
-				System.out.println("INF");
-				continue;
-			}
-			System.out.println(dist[i]);
-		}
-	}
-	static void dijkstra(int start) {
-		PriorityQueue<Vertex> queue = new PriorityQueue<>();
-		boolean[] v = new boolean[V + 1];
-		queue.add(new Vertex(start, 0));
-		dist[start] = 0;
-		while (!queue.isEmpty()) {
-			Vertex poll = queue.poll();
-			int minIdx = poll.e;
-			if (v[minIdx] == true) continue;
-			v[minIdx] = true;
-			for (Vertex next : vertexs[minIdx]) {
-				if (dist[next.e] > dist[minIdx] + next.w) {
-					dist[next.e] = dist[minIdx] + next.w;
-					queue.add(new Vertex(next.e, dist[next.e]));
-				}
-			}
-			
-			
-		}
-	}
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        V = sc.nextInt();
+        E = sc.nextInt();
+        K = sc.nextInt() - 1;
+        graph = new ArrayList<>();
+        int[] dis = new int[V];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        dis[K] = 0;
+        for (int i = 0; i < V; i++) {
+            graph.add(new ArrayList<>());
+        }
+        for (int i = 0; i < E; i++) {
+            int from = sc.nextInt();
+            int to = sc.nextInt();
+            int weight = sc.nextInt();
+            graph.get(from -1 ).add(new Edge(to - 1, weight));
+        }
+        PriorityQueue<Edge> q = new PriorityQueue<>((a, b) -> a.weight - b.weight);
+        q.add(new Edge(K, 0));
+        while (!q.isEmpty()) {
+            Edge poll = q.poll();
+            List<Edge> edges = graph.get(poll.vertex);
+            for (Edge edge : edges) {
+                if (dis[edge.vertex] > edge.weight + poll.weight) {
+                    dis[edge.vertex] = edge.weight + poll.weight;
+                    q.add(new Edge(edge.vertex, edge.weight + poll.weight));
+                }
+            }
+        }
+        for (int di : dis) {
+            if (di == Integer.MAX_VALUE) {
+                System.out.println("INF");
+            } else {
+                System.out.println(di);
+            }
+        }
+    }
+
+
+    static class Edge {
+        int vertex;
+        int weight;
+
+        public Edge(int vertex, int weight) {
+            this.vertex = vertex;
+            this.weight = weight;
+        }
+    }
 }
