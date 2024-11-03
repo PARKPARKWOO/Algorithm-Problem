0c6a0497691b12cdd0c67f4e562088f204464302
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-11-03 22:19:55 +0900

    [Silver III] Title: 퇴사, Time: 180 ms, Memory: 17744 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -1,39 +1,50 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-	static int N, ans;
-	static int[][] arr;
-	static int[] dp;
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		arr = new int[N][2];
-		dp = new int[N];
-		for (int i = 0; i < N; i++) {
-			arr[i][0] = sc.nextInt(); // 상담 완료 하는데 걸리는 기간 
-			arr[i][1] = sc.nextInt(); // 받을 수 있는 금액 
-		}
-		if (arr[0][0] < N + 1) {
-			dp[0] = arr[0][1];
-		}
-		for (int i = 1; i < N; i++) {
-			int temp = 0;
-			if (i + arr[i][0] < N + 1) {
-				temp = arr[i][1];
-				dp[i] = temp; // 현재 받을 수 있는 금액 	
-			}else continue;
-		
-			for (int j = i - 1; j >= 0; j--) {
-				if (arr[j][0] + j <= i) {
-					dp[i] = Math.max(dp[i], temp + dp[j]);
-				}
-			}
-		}
-		ans = Arrays.stream(dp).max().orElse(0);
-//		for (int d : dp) {
-//			ans = Math.max(ans, d);
-//		}
-		System.out.println(ans);
-	}
+    private static int N;
+    private static int[] dp;
+    private static List<Node> nodes;
+    private static int answer;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        dp = new int[N + 1];
+        nodes = new ArrayList<>();
+
+        for (int i = 0; i < N; i++) {
+            int t = sc.nextInt();
+            int p = sc.nextInt();
+            nodes.add(new Node(p, t, i));
+        }
+
+        for (int i = 0; i < N; i++) {
+            Node node = nodes.get(i);
+            if (node.canWork()) {
+                dp[node.idx + node.t] = Math.max(dp[node.idx + node.t], dp[i] + node.p);
+            }
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
+        }
+
+        answer = dp[N];
+        System.out.println(answer);
+
+        sc.close();
+    }
+
+    static class Node {
+        int p;
+        int t;
+        int idx;
+
+        public Node(int p, int t, int idx) {
+            this.p = p;
+            this.t = t;
+            this.idx = idx;
+        }
+
+        public boolean canWork() {
+            return idx + t <= N;
+        }
+    }
 }
