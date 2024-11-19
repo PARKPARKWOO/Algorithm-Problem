fa29be90f0e666c7991c6ee9ff5821c76ff74a19
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2024-11-20 01:06:56 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.01 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,43 +1,41 @@
 import java.util.*;
 class Solution {
-    private int[] dx = {-1, 1, 0, 0}; // 상 하 좌 우
-    private int[] dy = {0, 0, -1, 1};
-    private boolean[][] v;
-    private int result;
+    private int[] dx = {0, 0, 1, -1}; // 동 서 남 북
+    private int[] dy = {1, -1, 0, 0};
     public int solution(int[][] maps) {
-        int answer = 0;
-        result = 0;
-        int x = maps.length;
-        int y = maps[0].length;
-        v = new boolean[x][y];
-        bfs(maps, x, y);
-        return maps[x - 1][y - 1] == 1 ? -1 : maps[x-1][y-1];
-    }
-    private void bfs(int[][] maps, int xlen, int ylen) {
-        Queue<Nodes> q = new LinkedList<>();
-        q.add(new Nodes(0, 0));
+        int answer = Integer.MAX_VALUE;
+        boolean[][] v = new boolean[maps.length][maps[0].length];
+        
+        Queue<Node> q = new LinkedList<>();
+        q.add(new Node(0, 0, 1));
+        v[0][0] = true;
         while (!q.isEmpty()) {
-            Nodes poll = q.poll();
-            int x = poll.x;
-            int y = poll.y;
+            Node poll = q.poll();
+            if (poll.x == maps.length -1 && poll.y == maps[0].length - 1) {
+                answer = poll.cnt;
+                break;
+            }
             for (int i = 0; i < 4; i++) {
-                int nx = x + dx[i];
-                int ny = y + dy[i];
-                if (nx >= 0 && nx < xlen && ny >= 0 && ny < ylen && !v[nx][ny] && maps[nx][ny] != 0) {
-                    q.add(new Nodes(nx, ny));
+                int nx = dx[i] + poll.x;
+                int ny = dy[i] + poll.y;
+                if (nx >= 0 && ny >= 0 && nx < maps.length && ny < maps[0].length && !v[nx][ny] && maps[nx][ny] == 1) {
+                    q.add(new Node(nx, ny, poll.cnt + 1));
                     v[nx][ny] = true;
-                    maps[nx][ny] = maps[x][y] + 1;
                 }
             }
         }
+        return answer == Integer.MAX_VALUE ? -1 : answer;
     }
+    
 }
-class Nodes {
+
+class Node {
     int x;
     int y;
-    public Nodes(int x, int y) {
+    int cnt;
+    public Node(int x, int y, int cnt) {
         this.x = x;
         this.y = y;
-    
+        this.cnt = cnt;
     }
 }
