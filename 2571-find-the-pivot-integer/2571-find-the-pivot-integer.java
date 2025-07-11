f9129ecd18e00ff89250f295d85cc564259de800
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2025-07-11 23:54:20 +0900

    Time: 0 ms (100%), Space: 40.8 MB (54.28%) - LeetHub

--- 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
+++ 2571-find-the-pivot-integer/2571-find-the-pivot-integer.java
@@ -1,20 +1,9 @@
 class Solution {
     public int pivotInteger(int n) {
-        int sum = 0;
-        for (int i = 1; i <= n; i++) {
-            sum += i;
-        }
+        int sum= n*(n+1)/2;
+        double a= Math.sqrt(sum);
 
-        return index(n, sum, n);
-    }
-    // num 은 idx 에서 n 까지 합
-    private int index(int num, int sum, int idx) {
-        if (num > sum) {
-            return -1;
-        } else if (num < sum) {
-            int m = idx - 1;
-            return index(num + m, sum - idx, m);
-        }
-        return idx;
+        if( a- Math.ceil(a)==0) return (int)a;
+        else return -1;
     }
 }
