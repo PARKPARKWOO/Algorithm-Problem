2a746a1c18b40d4b46643b5a72589e22cc470b2f
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2025-07-29 22:25:00 +0900

    Time: 0 ms (100%), Space: 40.5 MB (43.01%) - LeetHub

--- 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.java
+++ 1761-count-sorted-vowel-strings/1761-count-sorted-vowel-strings.java
@@ -1,19 +1,20 @@
 class Solution {
-    private char[] elements = new char[]{'a', 'e', 'i', 'o', 'u'};
-    private Set<String> set = new HashSet<>();
+    private String[] constants = {"a", "e", "i", "o", "u"};
+    private int[] dp;
     public int countVowelStrings(int n) {
-        recursive(n, "", 0);
-        return set.size();
+        dp = new int[5];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            dp[0] = dp[0] + dp[1] + dp[2] + dp[3] + dp[4];
+            dp[1] = dp[1] + dp[2] + dp[3] + dp[4];
+            dp[2] = dp[2] + dp[3] + dp[4];
+            dp[3] = dp[3] + dp[4];
+        }
+        int answer = 0;
+        for (int d: dp) {
+            answer += d;
+        }
+        return answer;
     }
 
-    private void recursive(int n, String str, int idx) {
-        if (str.length() == n) {
-            set.add(str);
-            return;
-        }
-
-        for (int i = idx; i < elements.length; i++) {
-            recursive(n, str + elements[i], i);
-        }
-    }
 }
