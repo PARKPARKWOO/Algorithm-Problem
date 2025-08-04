15f3f019e0b2ee76bc1eac70c5a9551dd2f65f9b
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2025-08-04 21:40:41 +0900

    Time: 100 ms (5.33%), Space: 75.7 MB (5%) - LeetHub

--- 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/2379-maximum-total-importance-of-roads.java
@@ -1,34 +1,36 @@
 class Solution {
-    private Map<Integer, Integer> counter;
-    private Map<Integer, Integer> nums;
     public long maximumImportance(int n, int[][] roads) {
         long answer = 0;
-        counter = new HashMap<>();
-        nums = new HashMap<>();
-        for (int[] r: roads) {
-            int a = r[0];
-            int b = r[1];
+        Map<Integer, Integer> counter = new HashMap<>();
+        
+        // 각 노드의 연결 횟수 계산
+        for (int[] road : roads) {
+            int a = road[0];
+            int b = road[1];
             counter.put(a, counter.getOrDefault(a, 0) + 1);
             counter.put(b, counter.getOrDefault(b, 0) + 1);
         }
-
-        List<Map.Entry<Integer, Integer>> entries = new ArrayList<>(counter.entrySet());
-        entries.sort(
-            Map.Entry.<Integer, Integer>comparingByValue()
-             .reversed()
-        );
-        int cnt = n;
-        for (Map.Entry<Integer, Integer> entry : entries) {
-            Integer key = entry.getKey();
-            Integer value = entry.getValue();
-            nums.put(key, cnt--);
+        
+        // 모든 노드(0부터 n-1까지)를 연결 횟수로 정렬
+        List<Integer> nodes = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            nodes.add(i);
+        }
+        nodes.sort((a, b) -> counter.getOrDefault(b, 0) - counter.getOrDefault(a, 0));
+        
+        // 가장 많이 연결된 노드부터 높은 값 할당
+        Map<Integer, Integer> nodeValues = new HashMap<>();
+        for (int i = 0; i < nodes.size(); i++) {
+            nodeValues.put(nodes.get(i), n - i);
         }
         
-for (int[] road : roads) {
-    int a = road[0];
-    int b = road[1];
-    answer += nums.getOrDefault(a, 0) + nums.getOrDefault(b, 0);
-}
+        // 각 도로의 중요도 합계 계산
+        for (int[] road : roads) {
+            int a = road[0];
+            int b = road[1];
+            answer += nodeValues.get(a) + nodeValues.get(b);
+        }
+        
         return answer;
     }
 }
