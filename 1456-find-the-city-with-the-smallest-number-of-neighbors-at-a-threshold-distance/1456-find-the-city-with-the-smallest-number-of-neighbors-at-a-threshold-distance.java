4d756f54fbc0aec51a27f32fde9ad6b11a68310b
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2025-08-30 12:23:25 +0900

    Time: 148 ms (5.03%), Space: 45.7 MB (5.59%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,72 +1,55 @@
 class Solution {
-    private List<List<int[]>> graph;
-    private int[] dp;
-    private int answer;
-    private int minDepth = Integer.MAX_VALUE;
+    List<List<int[]>> graph = new ArrayList<>();
+    int[] dp;
+    int minCnt = Integer.MAX_VALUE;
+    int answer;
     public int findTheCity(int n, int[][] edges, int distanceThreshold) {
-        graph = new ArrayList<>();
-        dp = new int[n];
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
+        dp = new int[n];
+        Arrays.fill(dp, Integer.MAX_VALUE);
         for (int[] edge: edges) {
-            int from = edge[0];
-            int to = edge[1];
-            int weight = edge[2];
-            graph.get(from).add(new int[]{to, weight});
-            graph.get(to).add(new int[]{from, weight});
+            int f = edge[0];
+            int t = edge[1];
+            int w = edge[2];
+            graph.get(f).add(new int[]{t, w});
+            graph.get(t).add(new int[]{f, w});
         }
         for (int i = 0; i < n; i++) {
-            int cnt = dikjestra(i, distanceThreshold);
-            if (cnt <= minDepth) {
-                answer = i;
-                minDepth = cnt;
-            }
+            ds(i, distanceThreshold);
         }
         return answer;
     }
 
-    private int dikjestra(int start, int distanceThreshold) {
-        PriorityQueue<Nodes> q = new PriorityQueue<>();
-        int[] dp = new int[graph.size()];
-        Arrays.fill(dp, Integer.MAX_VALUE);
-        q.add(new Nodes(start, 0, 0));
-        dp[start] = 0;
-        int depth = 0;
-        int cnt = 0;
+    private void ds(int start, int dt) {
+        PriorityQueue<int[]> q = new PriorityQueue<>(
+            Comparator.comparingInt(arr -> arr[1])
+        );
+        // node depth cnt
+        q.add(new int[]{start, 0, 0});
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(start, 0);
+        int mc = 0;
         while (!q.isEmpty()) {
-            Nodes poll = q.poll();
-            if (poll.weight > dp[poll.idx]) continue;
-            List<int[]> edges = graph.get(poll.idx);
-            for (int[] edge: edges) {
-                int idx = edge[0];
-                int w = edge[1];
-                if (poll.weight + w < dp[idx] && distanceThreshold >= poll.weight + w) {
-                    q.add(new Nodes(idx, w + poll.weight, poll.depth + 1));
-                    dp[idx] =  w + poll.weight;
-                }
+            int[] poll = q.poll();
+            int depth = map.getOrDefault(poll[0], Integer.MAX_VALUE);
+            if (depth < poll[1] || poll[1] > dt) continue;
+            map.put(poll[0], poll[1]);
+            List<int[]> list = graph.get(poll[0]);
+            
+            for (int[] l : list) {
+                int sDis = l[1] + poll[1];
+                if (dt < sDis) continue;
+                q.add(new int[]{l[0], sDis, poll[2] + 1});
             }
         }
-        for (int d: dp) {
-            if (d != Integer.MAX_VALUE) {
-                cnt++;
-            }
+        mc = map.size() - 1;
+        if (mc < minCnt) {
+            answer = start;
+            minCnt = mc;
+        } else if (mc == minCnt && answer < start) {
+            answer = start;
         }
-        return cnt - 1;
-    }
-}
-
-class Nodes implements Comparable<Nodes>{
-    int idx;
-    int weight;
-    int depth;
-    public Nodes(int idx, int weight, int depth) {
-        this.idx = idx;
-        this.weight = weight;
-        this.depth = depth;
-    }
-
-    public int compareTo(Nodes o) {
-        return o.weight - this.weight;
     }
 }
