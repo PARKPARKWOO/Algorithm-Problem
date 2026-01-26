a15a06ef49790c955a437d1ec6e2030055e30f05
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-01-26 20:01:14 +0900

    Time: 2 ms (39.54%), Space: 44.9 MB (98.93%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -3,17 +3,12 @@ class Solution {
     boolean[] v;
     public List<List<Integer>> permute(int[] nums) {
         v = new boolean[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            v[i] = true;
-            List<Integer> list = new ArrayList<>();
-            list.add(nums[i]);
-            recursive(nums, list, i);   
-            v[i] = false;
-        }
+        List<Integer> list = new ArrayList<>();
+        recursive(nums, list);   
         return answer;
     }
 
-    private void recursive(int[] nums, List<Integer> list, int idx) {
+    private void recursive(int[] nums, List<Integer> list) {
         if (list.size() == nums.length) {
             List<Integer> copy = new ArrayList<>(list);
             answer.add(copy);
@@ -24,7 +19,7 @@ class Solution {
             if (!v[i]) {
                 v[i] = true;
                 list.add(nums[i]);
-                recursive(nums, list, i);
+                recursive(nums, list);
                 list.remove(list.size() - 1);
                 v[i] = false;
             }
