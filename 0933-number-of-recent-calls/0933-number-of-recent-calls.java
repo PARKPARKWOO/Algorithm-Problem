ade47e1e22c6e3fea2bff4a30f2d742f0b29f751
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-07-08 21:39:34 +0900

    Time: 63 ms (6.95%), Space: 58 MB (75.82%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -0,0 +1,31 @@
+class RecentCounter {
+    private int[] request;
+    private int idx;
+    private int start;
+    public RecentCounter() {
+        this.request = new int[10000];
+    }
+    
+    public int ping(int t) {
+        request[idx++] = t;
+        int end = idx;    
+        int cnt = 0;
+        int range = t - 3000;
+        for (int i = start; i < end; i++) {
+            if (request[i] >= range && request[i] <= t) {
+                cnt++;
+            } else {
+                start++;
+            }
+        }
+    
+        return cnt;
+    }
+    
+}
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
