3b6638c086418ec97e638abcfd1b5d6d92fbb8fc
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-07-12 15:28:11 +0900

    Time: 4 ms (94.83%), Space: 46.6 MB (91.05%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,45 +1,44 @@
 class Solution {
+    int[] answer;
+    List<Integer>[] graph;
+    int[] state;
     public int[] findOrder(int numCourses, int[][] prerequisites) {
-        int[] answer = new int[numCourses];
-        List<Integer>[] graph = new ArrayList[numCourses];
-        int[] state = new int[numCourses];
-        boolean[] v = new boolean[numCourses];
-        for (int i = 0; i < graph.length; i++) {
+        answer = new int[numCourses];
+        graph = new ArrayList[numCourses];
+        state = new int[numCourses];
+        Queue<Integer> q = new LinkedList<>();
+        int idx = 0;
+        for (int i = 0; i < numCourses; i++) {
             graph[i] = new ArrayList<>();
         }
-        
-        for (int i = 0; i < prerequisites.length; i++) {
-            int[] p = prerequisites[i];
+
+        for (int[] p: prerequisites) {
             int course = p[0];
             int pre = p[1];
-            
             graph[pre].add(course);
             state[course]++;
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        
-        for (int i = 0; i < numCourses; i++) {
-            if (state[i] == 0 && !v[i]) {
-                q.add(i);
-                v[i] = true;
-            }
+        for (int i = 0; i < state.length; i++) {
+            if (state[i] == 0) q.add(i);
         }
-        int depth = 0;
-        while (!q.isEmpty()) {
-            int poll = q.poll();
-            answer[depth++] = poll;
-            List<Integer> list = graph[poll];
 
-            for (int n : list) {
-                state[n]--;
-                if (state[n] == 0) {
-                    q.add(n);
-                    v[n] = true;
-                }
+        while (!q.isEmpty()) {
+            int poll =q.poll();
+            answer[idx++] = poll;
+            List<Integer> list = graph[poll];
+            
+            for (int i = 0; i < list.size(); i++) {
+                int value = list.get(i);
+                state[value]--;
+                if (state[value] == 0) q.add(value);
             }
         }
-        if (depth != numCourses) return new int[0];
-        return answer;
+
+        for (int i : state) {
+            if (i != 0) return new int[0];
+        }
+
+        return answer; 
     }
 }
