0f1596704abc1af94f1fca7c61ecfc091c97ddcc
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-07-16 12:21:08 +0900

    Time: 15 ms (72.04%), Space: 52.9 MB (67.96%) - LeetHub

--- 0811-subdomain-visit-count/0811-subdomain-visit-count.java
+++ 0811-subdomain-visit-count/0811-subdomain-visit-count.java
@@ -1,35 +1,30 @@
 class Solution {
-    Map<String, Integer> map;
+    List<String> answer = new ArrayList<>();
+    Map<String, Integer> map = new HashMap<>();
     public List<String> subdomainVisits(String[] cpdomains) {
-        map = new HashMap<>();
-        List<String> answer = new ArrayList<>();
-        for (int i = 0; i < cpdomains.length; i++) {
-            String s = cpdomains[i];
+        for (String s: cpdomains) {
             String[] split = s.split(" ");
-            int visit = Integer.valueOf(split[0]);
             String domain = split[1];
-            
-            parse(domain, visit);
+            int cnt = Integer.valueOf(split[0]);
+            parse(domain, cnt);
         }
 
         for (Map.Entry<String, Integer> entry: map.entrySet()) {
             String key = entry.getKey();
             int value = entry.getValue();
-            String s = value +" " + key;
-            answer.add(s);
+            answer.add(value + " " + key);
         }
-        return answer;        
+        return answer;
     }
-    
-    void parse(String domain, int visit) {
-        String[] split = domain.split("\\.");
+    void parse(String domain, int cnt) {
+        String[] d = domain.split("\\.");
         
-        for (int i = 0; i < split.length; i++) {
-            String s = split[i];
-            for (int j = i + 1; j < split.length; j++) {
-                s += "." + split[j];
+        for (int i = 0; i < d.length; i++) {
+            String s = d[i];
+            for (int j = i + 1; j < d.length; j++) {
+                s += "." + d[j];
             }
-            map.put(s, map.getOrDefault(s, 0) + visit);
+            map.put(s, map.getOrDefault(s, 0) + cnt);
         }
     }
 }
