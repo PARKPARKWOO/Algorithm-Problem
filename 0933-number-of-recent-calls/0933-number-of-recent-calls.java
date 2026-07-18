3e70232217ba0298428f2b6c4d96b0df200f61ec
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-07-18 13:51:35 +0900

    Time: 22 ms (55.52%), Space: 59.5 MB (52.18%) - LeetHub

--- 0933-number-of-recent-calls/0933-number-of-recent-calls.java
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.java
@@ -1,30 +1,17 @@
 class RecentCounter {
-    int[] requests = new int[10001];
-    int idx;
-    int offset = 3000;
-    int startIdx;
+    Queue<Integer> q = new LinkedList<>();
     public RecentCounter() {
         
     }
     
     public int ping(int t) {
-        requests[idx++] = t;
-        int start = t - offset;
-        if (start < 0) start = 0;
-        int s = startIdx;
-        int e = idx - 1;
-        while (s <= e) {
-            int mid = (s + e) / 2;
-            int v = requests[mid];
-            if (v >= start) {
-                e = mid - 1;
-                startIdx = mid;
-            } else {
-                s = mid + 1;
-            }
+        q.add(t);
+        
+        while (!q.isEmpty() && q.peek() < t - 3000) {
+            q.poll();
         }
 
-        return idx - startIdx;
+        return q.size();
     }
 }
 
