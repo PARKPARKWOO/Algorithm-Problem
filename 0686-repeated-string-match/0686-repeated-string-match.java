b4b64424b50e624995f8aa1e1e5819cb21976625
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-07-20 12:54:39 +0900

    Time: 8 ms (92.23%), Space: 43.4 MB (81.64%) - LeetHub

--- 0686-repeated-string-match/0686-repeated-string-match.java
+++ 0686-repeated-string-match/0686-repeated-string-match.java
@@ -4,18 +4,45 @@ class Solution {
     public int repeatedStringMatch(String a, String b) {
         aLen = a.length();
         bLen = b.length();
+        
         int repeatCnt = (aLen + bLen - 1) / aLen;
 
         StringBuilder sb = new StringBuilder();
+        
         for (int i = 0; i < repeatCnt; i++) {
             sb.append(a);
         }
 
-        if (sb.indexOf(b) != -1) return repeatCnt;
+        if (contains(sb.toString(), b)) return repeatCnt;
         
         sb.append(a);
-        if (sb.indexOf(b) != -1) return repeatCnt + 1;
+        if (contains(sb.toString(), b)) return repeatCnt + 1;
         
         return -1;
+    }
+    boolean contains(String text, String pattern) {
+        int windowHash = 0;
+        int patternHash = 0;
+        
+        for (int i = 0; i < pattern.length(); i++) {
+            windowHash += text.charAt(i);
+            patternHash += pattern.charAt(i);    
+        }
+
+        for (int i = 0; i <= text.length() - pattern.length(); i++) {
+            if (windowHash == patternHash && isMatch(text, pattern, i)) return true;
+            if (i == text.length() - pattern.length()) break;
+            windowHash -= text.charAt(i);
+            windowHash += text.charAt(i + pattern.length());
+        }
+        return false;
+    }
+
+    boolean isMatch(String text, String pattern, int start) {
+        for (int i = 0; i < pattern.length(); i++) {
+            if (text.charAt(start + i) != pattern.charAt(i)) return false;
+        }
+
+        return true;
     }
 }
