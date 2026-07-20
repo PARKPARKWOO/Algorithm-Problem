827a0421aee735bf1b4881bedf7d0186e1bfb309
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-07-20 14:56:00 +0900

    Time: 4 ms (15.86%), Space: 49.5 MB (11.74%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,30 +1,64 @@
 class Solution {
-    private boolean[] v;
-    private int answer;
+    List<List<Integer>> graph = new ArrayList<>();
+    int[] parent;
+    int[] size;
+    boolean[] v;
+    int answer = 0;
     public int findCircleNum(int[][] isConnected) {
+        parent = new int[isConnected.length];
+        size = new int[isConnected.length];
         v = new boolean[isConnected.length];
         for (int i = 0; i < isConnected.length; i++) {
-            bfs(isConnected, i);
+            graph.add(new ArrayList<>());
         }
-        return answer;
+        
+        for (int i = 0; i <isConnected.length; i++) {
+            int[] c = isConnected[i];
+            parent[i] = i;    
+            for (int j = 0; j <c.length; j++) {
+                if (j == i || c[j] == 0) continue;
+            
+                graph.get(i).add(j);
+            }
+        }
+        for (int i = 0; i < graph.size(); i++) {
+            if (!v[i]){
+                bfs(i, isConnected);
+            }
+        }
+        
+        return answer;   
     }
     
-    private void bfs(int[][] isConnected, int start) {
-        if (v[start]) {
-            return;
-        }
-        answer++;
+    void bfs(int x, int[][] isCon) {
+        v[x] = true;
         Queue<Integer> q = new LinkedList<>();
-        q.add(start);
+        q.add(x);
+        answer++;
         while (!q.isEmpty()) {
             int poll = q.poll();
-            int[] arr = isConnected[poll];
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == 1 && !v[i]) {
-                    v[i] = true;
-                    q.add(i);
+            List<Integer> list = graph.get(poll);
+            for (int i = 0; i <list.size(); i++) {
+                int get = list.get(i);
+                if (!v[get]){
+                    v[get] =true;
+                    q.add(get);
                 }
             }
+        }
+    }
+
+    int find(int x) {
+        if (parent[x] == x) return x;
+        return parent[x];
+    }
+
+    void union(int a, int b) {
+        int aP = find(a);
+        int bP = find(b);
+        
+        if (size[aP] < size[bP]) {
+            
         }
     }
 }
