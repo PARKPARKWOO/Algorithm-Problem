d3d65fde3e7e4c936e8508545aa3dccac6038531
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-06 11:54:45 +0900

    Time: 10 ms (21.41%), Space: 49 MB (60.5%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,20 +1,20 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        int[] pre = intervals[0]; 
-        List<int[]> list = new ArrayList<>();
+        List<int[]> merge = new ArrayList<>();
+        int[] pre = new int[2];
+        pre = intervals[0];
         for (int i = 1; i < intervals.length; i++) {
             int[] inter = intervals[i];
+            // pre 의 값의 범위 안에 [0] 의 값이 포함된다면 merge 를 진행한다.
             if (pre[1] >= inter[0]) {
                 pre[1] = Math.max(pre[1], inter[1]);
             } else {
-                list.add(pre);
+                merge.add(pre);
                 pre = inter;
             }
         }
-
-        list.add(pre);
-        return list.toArray(new int[list.size()][]);
+        merge.add(pre);
+        return merge.toArray(new int[merge.size()][]);
     }
 }
