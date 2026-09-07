27d481715c08685c3e16e11b4df7ff33e6b860d6
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-07 21:17:33 +0900

    Time: 7 ms (14.59%), Space: 51.4 MB (85.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,37 +1,38 @@
 class Solution {
-    int[] dx = {-1, 1, 0, 0}; // 상 하 좌 우
-    int[] dy = {0, 0, -1, 1};
-    int answer;
+    boolean[][] v;
+    int answer = 0;
+    int[] dx = new int[]{-1, 1, 0, 0}; // 상 하 좌 우
+    int[] dy = new int[]{0, 0, -1, 1};
     public int numIslands(char[][] grid) {
+        v = new boolean[grid.length][grid[0].length]; 
         
         for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j <grid[0].length; j++) {
-                if (grid[i][j] == '1') {
-                    bfs(grid, i , j);
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1' && !v[i][j]) {
                     answer++;
+                    bfs(i, j, grid);
                 }
             }
         }
         return answer;
     }
 
-    void bfs(char[][] grid, int x, int y) {
+    void bfs(int x, int y, char[][] grid) {
         Queue<Node> q = new LinkedList<>();
-        q.add(new Node(x, y));
-        
+        q.add(new Node(x, y ));
+        v[x][y] = true;
         while (!q.isEmpty()) {
             Node poll = q.poll();
-            
             for (int i = 0; i < 4; i++) {
-                int nx = dx[i] + poll.x;
-                int ny = dy[i] + poll.y;
+                int nx = poll.x + dx[i];
+                int ny = poll.y + dy[i];
                 if (
-                    nx >= 0 && ny >= 0 &&
+                    nx >= 0 && ny >= 00 &&
                     nx < grid.length && ny < grid[0].length &&
-                    grid[nx][ny] == '1'
+                    grid[nx][ny] == '1' && !v[nx][ny]
                 ) {
                     q.add(new Node(nx, ny));
-                    grid[nx][ny] = '0';
+                    v[nx][ny] = true;
                 }
             }
         }
@@ -41,7 +42,7 @@ class Solution {
 class Node {
     int x;
     int y;
-    public Node(int x, int y ){ 
+    public Node(int x, int y) {
         this.x = x;
         this.y = y;
     }
