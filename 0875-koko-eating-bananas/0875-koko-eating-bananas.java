36ce93e767024291ebde4e362c418a9c3897810f
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-12 18:15:46 +0900

    Time: 12 ms (49.81%), Space: 47.8 MB (57.63%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,33 +1,35 @@
 class Solution {
-    int answer = Integer.MAX_VALUE;
     public int minEatingSpeed(int[] piles, int h) {
-        int min = 1;
+        int answer = Integer.MAX_VALUE;
         int max = 0;
+        int min = 1;
         for (int p : piles) {
-            max = Math.max(p , max);
+            max = Math.max(p, max);
         }
-        
+
         while (min <= max) {
-            int mid = min + (max - min) / 2;
-            boolean can = recur(piles, h, mid);
+            int k = min + (max - min) / 2;
+            
+            boolean can = can(piles, h, k);
             if (can) {
-                max = mid - 1;
-                answer = Math.min(answer, mid);
+                max = k - 1;
+                answer = Math.min(answer, k);
             } else {
-                min = mid + 1;
+                min = k + 1;
             }
         }
-        
-        return answer;
+        return answer; 
     }
 
-    boolean recur(int[] piles, int h,int k) {
+    boolean can(int[] piles, int h, int k) {
         long time = 0;
+        
         for (int i = 0; i < piles.length; i++) {
             int p = piles[i];
             time += p / k;
             if (p % k != 0) time++;
         }
+
         return time <= h;
     }
 }
