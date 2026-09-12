b7f1005a5f2f2d923f8df63bbeb0ea00bb41a4bd
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-12 17:41:43 +0900

    Time: 6 ms (21.11%), Space: 51.2 MB (92.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,49 +1,40 @@
 class Solution {
-    boolean[][] v;
-    int answer = 0;
     int[] dx = new int[]{-1, 1, 0, 0}; // 상 하 좌 우
     int[] dy = new int[]{0, 0, -1, 1};
+    boolean[][] v;
+    int answer;
     public int numIslands(char[][] grid) {
-        v = new boolean[grid.length][grid[0].length]; 
-        
-        for (int i = 0; i < grid.length; i++) {
-            for (int j = 0; j < grid[0].length; j++) {
-                if (grid[i][j] == '1' && !v[i][j]) {
-                    answer++;
-                    bfs(i, j, grid);
+        v = new boolean[grid.length][grid[0].length];
+        for (int x = 0; x < grid.length; x++) {
+            for (int y = 0; y < grid[x].length; y++) {
+                if (!v[x][y] && grid[x][y] == '1') {
+                    bfs(grid, x, y);
                 }
             }
         }
         return answer;
     }
 
-    void bfs(int x, int y, char[][] grid) {
-        Queue<Node> q = new LinkedList<>();
-        q.add(new Node(x, y ));
+    void bfs(char[][] grid, int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
         v[x][y] = true;
+        answer++;
         while (!q.isEmpty()) {
-            Node poll = q.poll();
+            int[] poll = q.poll();
+
             for (int i = 0; i < 4; i++) {
-                int nx = poll.x + dx[i];
-                int ny = poll.y + dy[i];
+                int nx = poll[0] + dx[i];
+                int ny = poll[1] + dy[i];
                 if (
-                    nx >= 0 && ny >= 00 &&
-                    nx < grid.length && ny < grid[0].length &&
-                    grid[nx][ny] == '1' && !v[nx][ny]
+                    nx >= 0 && nx < grid.length &&
+                    ny >= 0 && ny < grid[0].length &&
+                    !v[nx][ny] && grid[nx][ny] == '1'
                 ) {
-                    q.add(new Node(nx, ny));
                     v[nx][ny] = true;
+                    q.add(new int[]{nx, ny});
                 }
             }
         }
-    }
-}
-
-class Node {
-    int x;
-    int y;
-    public Node(int x, int y) {
-        this.x = x;
-        this.y = y;
     }
 }
