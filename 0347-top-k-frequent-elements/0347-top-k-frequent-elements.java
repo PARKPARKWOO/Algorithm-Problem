9ad5ccb38ccc9b1f4a38057f09bb0532b7dc2193
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-12 17:13:42 +0900

    Time: 16 ms (38.99%), Space: 47.7 MB (33.37%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,16 +1,18 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int[] answer = new int[k];
         Map<Integer, Integer> map = new HashMap<>();
+        int[] answer = new int[k];
         for (int n : nums) {
             map.put(n, map.getOrDefault(n, 0) + 1);
         }
-        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(map.entrySet());
-        entryList.sort(Map.Entry.comparingByValue());
+        List<Map.Entry<Integer,Integer>> list = new ArrayList<>(map.entrySet());
+        list.sort(Map.Entry.comparingByValue());
         int idx = 0;
-        for (int i = entryList.size() - 1; i >= entryList.size() - k; i--) {
-            answer[idx++] = entryList.get(i).getKey();
+        for (int i = list.size() - 1; i > list.size() - k - 1; i--) {
+            Map.Entry<Integer, Integer> entry = list.get(i);
+            answer[idx++] = entry.getKey(); 
         }
+
         return answer;
     }
 }
