5153f628262763c63a131422c56800bfeb100d05
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-12 15:03:47 +0900

    Time: 44 ms (53.4%), Space: 48.3 MB (5.05%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,22 +1,17 @@
-
-
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        // 중복문자열이 있는지 확인 해야한다.
-        // 길이에 대한 정보를 알아야 한다. (중복 문자열에 대한 유효한 길ㅇ ㅣ확인)
         Map<Character, Integer> map = new HashMap<>();
-        int answer = 0;
-        int currentLen = 0;
+
         int left = 0;
-        for (int i = 0; i < s.length(); i++) {
-            char c = s.charAt(i);
-            if (!map.containsKey(c) || left > map.get(c)) {
-                answer = Math.max(i - left + 1, answer);
-            } else {
-                left = map.get(c) + 1;
-                       
+        int answer = 0;
+        for (int right = 0; right < s.length(); right++) {
+            char c = s.charAt(right);
+            if (map.containsKey(c) && left <= map.get(c)) {
+                int idx = map.get(c);
+                left = idx + 1;
             }
-            map.put(c, i);
+            map.put(c, right);
+            answer = Math.max(answer, right - left + 1);
         }
         return answer;
     }
