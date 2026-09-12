61bf52233c688bad24165acae6837502dd25d804
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-12 17:58:43 +0900

    Time: 6 ms (72.47%), Space: 47.1 MB (48.44%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,36 +1,39 @@
 class Solution {
-    List<List<Integer>> graph = new ArrayList<>();
-    int[] indegree;
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
+        List<List<Integer>> graph = new ArrayList<>();
+        
         for (int i = 0; i < numCourses; i++) {
             graph.add(new ArrayList<>());
         }
-        indegree = new int[numCourses];
-        
-        for (int[] pre : prerequisites) {
-            int require = pre[1];
+
+        for (int[] pre: prerequisites) {
+            int pr = pre[1];
             int course = pre[0];
-            graph.get(course).add(require);
-            indegree[require]++;
+            
+            graph.get(pr).add(course);
+            indegree[course]++;
+        }
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < numCourses; i++){
+            if (indegree[i] == 0) {
+                q.add(i);
+            }
         }
 
-        Queue<Integer> q = new LinkedList<>();
-
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) q.add(i);
-        }        
-        
         while (!q.isEmpty()) {
             int poll = q.poll();
+            
             List<Integer> list = graph.get(poll);
-            for (int i: list) {
+            
+            for (int i : list) {
                 indegree[i]--;
-                if (indegree[i] == 0) q.add(i);
+                if (indegree[i] <= 0) q.add(i);
             }
         }
 
         for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] != 0) return false;
+            if (indegree[i] > 0) return false;
         }
         return true;
     }
