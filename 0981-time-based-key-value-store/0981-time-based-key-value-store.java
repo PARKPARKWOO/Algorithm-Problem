778f1decc482bad4311559c2779644e894b37333
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-12 18:09:49 +0900

    Time: 134 ms (66.81%), Space: 108 MB (80.17%) - LeetHub

--- 0981-time-based-key-value-store/0981-time-based-key-value-store.java
+++ 0981-time-based-key-value-store/0981-time-based-key-value-store.java
@@ -1,50 +1,50 @@
 class TimeMap {
-    Map<String, List<Node>> map = new HashMap<>();
+    Map<String, List<Data>> map;
     public TimeMap() {
+        this.map = new HashMap<>();
         
     }
     
     public void set(String key, String value, int timestamp) {
-        Node node = new Node(value, timestamp);
-        List<Node> list = this.map.get(key);
-        if (list == null) {
-            list = new ArrayList<>();
-        }
-        list.add(node);
+        Data data = new Data(value, timestamp);
+        
+        List<Data> list = map.get(key);
+        if (list == null) list = new ArrayList<>();
+        list.add(data);
         map.put(key, list);
     }
     
     public String get(String key, int timestamp) {
-        List<Node> list = map.get(key);
-        if (list != null) {
-            int right = list.size() - 1;
+        // "" 이 반환되는 케이스 = 1. 키 값이 없음 2. 데이터는 있지만 시간이 지났음
+        String result = "";
+        if (map.containsKey(key)) {
+            List<Data> list = map.get(key);
             int left = 0;
-            String answer = "";
+            int right = list.size() - 1;
+            
             while (left <= right) {
                 int mid = left + (right - left) / 2;
-                Node get = list.get(mid);
-                if (get.time <= timestamp) {
+                Data data = list.get(mid);
+                if (data.time <= timestamp) {
                     left = mid + 1;
-                    answer = get.value;
+                    result = data.value;
                 } else {
                     right = mid - 1;
                 }
             }
-            return answer;
         }
-        return "";
+
+        return result;
     }
 }
-
-class Node {
+class Data {
     String value;
     int time;
-    public Node(String value, int time) {
+    public Data(String value, int time){
         this.value = value;
         this.time = time;
     }
 }
-
 /**
  * Your TimeMap object will be instantiated and called as such:
  * TimeMap obj = new TimeMap();
