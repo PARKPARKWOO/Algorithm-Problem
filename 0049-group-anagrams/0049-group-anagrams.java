7c74f1f181265130d1ec9cb6e90f6ea2e6178ef9
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-13 12:32:22 +0900

    Time: 7 ms (71.8%), Space: 49.7 MB (45.67%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,21 +1,25 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
+        // 요소들의 알파벳 순서와 상관없이 동일한 단어라면 group 화를 시키는 것
+        // M * n log n 을 만족하는 방식
+        List<List<String>> answer = new ArrayList<>();
         Map<String, List<String>> map = new HashMap<>();
         for (int i = 0; i < strs.length; i++) {
             String str = strs[i];
-            char[] ca = str.toCharArray();
-            Arrays.sort(ca);
-            String newString = new String(ca);
-            List<String> list = map.get(newString);
+            char[] charArr = str.toCharArray();
+            // eat, tae -> aet
+            Arrays.sort(charArr);
+            String sortStr = new String(charArr);
+            List<String> list = map.get(sortStr);
             if (list == null) list = new ArrayList<>();
             list.add(str);
-            map.put(newString, list);
+            map.put(sortStr, list);
         }
-        List<List<String>> answer = new ArrayList<>();
-        
-        for (Map.Entry<String, List<String>> entry : map.entrySet()) {
+
+        for (Map.Entry<String, List<String>> entry: map.entrySet()) {
             answer.add(entry.getValue());
         }
+
         return answer;
     }
 }
