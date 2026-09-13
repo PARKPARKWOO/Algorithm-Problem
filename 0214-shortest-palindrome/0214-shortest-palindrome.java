389278aaeee799f6958cdebe5efa5e9649ed8415
박우영 <118690612+PARKPARKWOO@users.noreply.github.com>
2026-09-13 12:44:23 +0900

    Time: 4 ms (97.01%), Space: 46.8 MB (61.63%) - LeetHub

--- 0214-shortest-palindrome/0214-shortest-palindrome.java
+++ 0214-shortest-palindrome/0214-shortest-palindrome.java
@@ -1,12 +1,17 @@
 class Solution {
     public String shortestPalindrome(String s) {
+        // 문자열 s 의 prefix 에 부족한 부ㅜㅂㄴ을 채워 넣어야한다.
+        // s 의 앞 뒤 중 어디까지가 일치하는가?
         int left = 0;
         
         for (int right = s.length() - 1; right >= 0; right--) {
             if (s.charAt(left) == s.charAt(right)) left++;
         }
-        if (left == s.length()) return s;
+
+        if (s.length() == left) return s;
+
         String suffix = s.substring(left, s.length());
+        
         return new StringBuffer(suffix).reverse() + shortestPalindrome(s.substring(0, left)) + suffix;
     }
 }
